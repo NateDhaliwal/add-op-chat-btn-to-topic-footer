fd5aa1f8af7999426a2be8cdaa0f27fedb9d9693
NateDhaliwal <143097364+NateDhaliwal@users.noreply.github.com>
2025-01-11 23:07:25 +0800

    Update and rename create-chat-button.gjs to create-chat-button.js

--- javascripts/components/create-chat-button.js
+++ javascripts/components/create-chat-button.js
@@ -8,22 +8,13 @@ export default apiInitializer("1.14.0", (api) => {
   api.onPageChange((url, title) => {
     class CreateChatButton extends Component {
       @action
-      getOPPosterFromTopic() {
+      async getOPPosterFromTopic() {
         if (url.includes('/t/')) {
-          fetch(url + ".json").then((result) => return result["post_stream"]["posts"][0]["username"]);
+          await fetch(url + ".json").then((result) => return result["post_stream"]["posts"][0]["username"]);
         } else {
           return;
         }
       }
-      <template>
-        <p>Hi</p>
-        <DButton
-          class="btn btn-primary btn-text"
-          @action={{this.getOPPosterFromTopic}}
-          @icon="eye"
-          @label={{theme-prefix "chat_with_op"}}
-        />
-      </template>
     };
   });
 });
