bb33b55958147ac07be9ae5bda665b5c71c1cd7b
NateDhaliwal <143097364+NateDhaliwal@users.noreply.github.com>
2025-01-12 19:20:21 +0800

    Update create-chat-button.js

--- javascripts/components/create-chat-button.js
+++ javascripts/components/create-chat-button.js
@@ -5,26 +5,27 @@ import { action } from '@ember/object';
 import { inject as service } from "@ember/service";
 
 export default apiInitializer("1.14.0", (api) => {
-  api.onPageChange((url, title) => {
     class CreateChatButton extends Component {
-      @action
-      getOPPosterFromTopic() {
-        if (url.includes('/t/')) {
-          let Op;
-          let Opp;
-          Op = fetch(`${url}.json`).then((result) => {
-            /*result["post_stream"]["posts"][0]["username"]);*/
-            Opp = 'Hi';
-            console.log(Op);
-            console.log(Opp);
-            return 'hi';
-          });
-          window.location.href=`/chat/new-message?recipients=${Op}`;
-          return Op;
-        } else {
-          return;
+      api.onPageChange((url, title) => {
+        @action
+        getOPPosterFromTopic() {
+          
+          if (url.includes('/t/')) {
+            let Op;
+            let Opp;
+            Op = fetch(`${url}.json`).then((result) => {
+              /*result["post_stream"]["posts"][0]["username"]);*/
+              Opp = 'Hi';
+              console.log(Op);
+              console.log(Opp);
+              return 'hi';
+            });
+            window.location.href=`/chat/new-message?recipients=${Op}`;
+            return Op;
+          } else {
+            return;
+          }
         }
-      }
+      });
     };
-  });
 });
