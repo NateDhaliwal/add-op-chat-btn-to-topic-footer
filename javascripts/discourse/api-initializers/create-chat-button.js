190920a304c9c4a9013d57278c7ab47df1aacc35
NateDhaliwal <143097364+NateDhaliwal@users.noreply.github.com>
2025-01-15 18:38:11 +0800

    Update create-chat-button.js

--- javascripts/discourse/api-initializers/create-chat-button.js
+++ javascripts/discourse/api-initializers/create-chat-button.js
@@ -18,7 +18,8 @@ export default apiInitializer("1.14.0", (api) => {
           return settings.topic_group_button_title;
         },
         action() {
-            console.log(`This: ${this}, topicid: ${this.get("topic.id")}`)
+            console.log(`This: ${this}, topicid: ${this.get("topic.id")}`);
+            window.location.href = `/t/${this.get("topic.id")}`;
         },
         dropdown() {
           return this.site.mobileView;
